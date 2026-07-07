bf384718882d6c68e4b754b30dc79ce148e6d582
tom6174 <93242765+tom6174@users.noreply.github.com>
2026-07-07 11:41:42 +0900

    chore: add script to update 2025 scholarships approval date

---
+++ supabase/migrations/045_update_2025_scholarships_approval_date.sql
@@ -0,0 +1,6 @@
+-- 045_update_2025_scholarships_approval_date.sql
+-- 1차년도(2025) 장학금 데이터의 승인일을 '2025-12-31'로 일괄 할당
+
+UPDATE public.scholarships
+SET approval_date = '2025-12-31'
+WHERE year = 2025;
